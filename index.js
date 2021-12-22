1e1b31605f6e5e81715d3930d078a304af4f5097
Yifan Xu <yifanxu00@outlook.com>
2021-12-21 22:32:54 -0500

    initial commit

---
+++ index.js
@@ -0,0 +1,113 @@
+const { init, request } = require('./riotreq.js')
+const apikey = require('./apikey.json')
+const fs = require('fs')
+
+init(apikey.key, 'na1', 'americas')
+
+const matchHistoryCount = 20
+
+async function spiderSearch (initPlayer, maxMatch = 1000) {
+  const initPlayerInfo = await request(`/lol/summoner/v4/summoners/by-name/${initPlayer}`)
+
+  const searchedMatches = []
+  var failFlag = false
+  let totalPlayerScanned = 0
+
+  const playerQueueRoot = {
+    puuid: initPlayerInfo.puuid,
+    next: null
+  }
+
+  let currentPlayer = playerQueueRoot
+
+  let aggregate = {}
+
+  while (!failFlag && searchedMatches.length <= maxMatch && currentPlayer) {
+    // Get Match IDs
+    const matches = await request(`/lol/match/v5/matches/by-puuid/${currentPlayer.puuid}/ids?queue=420&start=0&count=${matchHistoryCount}`, true)
+    const promises = matches.map(async matchId => {
+      if (searchedMatches.includes(matchId) || searchedMatches.length > maxMatch) {
+        console.log(`Skipping ${matchId}`)
+        return null
+      }
+      searchedMatches.push(matchId)
+
+      // Get Match Data
+      const matchInfo = await request(`/lol/match/v5/matches/${matchId}`, true)
+
+      // Aggregate Data and get players
+      matchInfo.info.participants.forEach(participantInfo => {
+        const champ = participantInfo.championName
+        
+        // Aggregate Data
+        if (aggregate[champ]) {
+          aggregate[champ].total++
+          if(participantInfo.win) aggregate[champ].win++
+        }
+        else {
+          aggregate[champ] = {
+            total: 1,
+            win: participantInfo.win ? 1 : 0
+          }
+        }
+
+        // Get more player
+        const newId = participantInfo.puuid
+        let isRepeat = false
+        let n = playerQueueRoot
+        while (n.next) {
+          if(n.puuid === newId) {
+            isRepeat = true
+            break
+          }
+          n = n.next
+        }
+        if(!isRepeat && n.puuid !== newId) {
+          n.next = {
+            puuid: newId,
+            next: null
+          }
+        }
+      })
+    })
+
+    await Promise.all(promises)
+    .catch(err => {
+      console.error(err)
+      failFlag = true
+    })
+
+    currentPlayer = currentPlayer.next
+    totalPlayerScanned++
+    console.log(`Scanned ${searchedMatches.length} matches and ${totalPlayerScanned} players`)
+  }
+
+  Object.keys(aggregate).forEach(champName => aggregate[champName].winRate = aggregate[champName].win / aggregate[champName].total)
+
+  return {
+    totalPlayerScanned,
+    matchesScanned: searchedMatches.length,
+    aggregate
+  }
+}
+
+// request('/lol/summoner/v4/summoners/by-name/Ginsu')
+// .then(res => {
+//   const puuid = res.puuid
+//   return request(`/lol/match/v5/matches/by-puuid/${puuid}/ids`, true)
+// })
+// .then(res => {
+//   return Promise.all(res.map(matchId => {
+//     return request(`/lol/match/v5/matches/${matchId}`, true)
+//   }))
+// })
+// .then(res => {
+//   const str = JSON.stringify(res, null, 2)
+//   fs.writeFileSync('./output.json', str)
+// })
+
+spiderSearch('Ginsu')
+.then(res => {
+  const str = JSON.stringify(res, null, 2)
+  fs.writeFileSync('./output.json', str)
+})
---
+++ riotreq.js
@@ -0,0 +1,66 @@
+const axios = require('axios')
+var AUTH_KEY
+var plathost
+var reghost
+
+const minReqInterval = 1500
+
+var lastPlatformRequest = 0
+var lastRegionRequest = 0
+
+var startTime
+
+function init (key, platform, region) {
+  AUTH_KEY = key,
+  plathost = `https://${platform}.api.riotgames.com`
+  reghost = `https://${region}.api.riotgames.com`
+  startTime = Date.now()
+}
+
+async function request (endpoint, isRegional = false) {
+
+  let time = Date.now();
+  let lastReq = (isRegional ? lastRegionRequest : lastPlatformRequest)
+
+  // console.log(`QUEUED     ${endpoint} at t=${time - startTime} (lastReq=${(isRegional ? lastRegionRequest : lastPlatformRequest) - startTime}) (REGIONAL = ${isRegional})`)
+
+  while (time - minReqInterval + 10 < lastReq) {
+    // console.log(`DELAYED    ${endpoint} at t=${time - startTime} (lastReq=${(isRegional ? lastRegionRequest : lastPlatformRequest) - startTime}) (REGIONAL = ${isRegional})`)
+    await sleep(minReqInterval)
+    time = Date.now()
+    lastReq = (isRegional ? lastRegionRequest : lastPlatformRequest)
+  }
+
+  console.log(`REQUESTING ${endpoint} at t=${time - startTime} (lastReq=${(isRegional ? lastRegionRequest : lastPlatformRequest) - startTime}) (REGIONAL = ${isRegional})`)
+
+  if(isRegional) {
+    lastRegionRequest = time
+  }
+  else {
+    lastPlatformRequest = time
+  }
+
+  // return endpoint
+  
+  const options = {
+    method: 'GET',
+    headers: { 
+      "X-Riot-Token": AUTH_KEY
+    },
+    url: (isRegional ? reghost: plathost) + endpoint,
+  }
+  
+  return axios(options)
+    .then(res => res.data)
+}
+
+function sleep(ms) {
+  return new Promise((resolve) => {
+    setTimeout(resolve, ms);
+  });
+}
+
+module.exports = {
+  init,
+  request: request
+}
