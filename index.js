4f510db170276c9bd72eb5f10bc3770651691c60
YifanXu <yifanxu00@outlook.com>
2021-12-22 17:06:17 -0800

    Provide more detailed information on champion winrates, including against specific champions

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ async function spiderSearch (initPlayer, maxMatch) {
 
   let aggregate = {}
 
-  while (!failFlag && searchedMatches.length <= maxMatch && currentPlayer) {
+  while (!failFlag && searchedMatches.length < maxMatch && currentPlayer) {
     // Get Match IDs
     const matches = await request(`/lol/match/v5/matches/by-puuid/${currentPlayer.puuid}/ids?${config.queueType === -1 ? "" : (`queueType=${config.queueType}&`)}start=0&count=${config.matchHistoryCount}`, true)
     const promises = matches.map(async matchId => {
@@ -34,21 +34,83 @@ async function spiderSearch (initPlayer, maxMatch) {
       // Get Match Data
       const matchInfo = await request(`/lol/match/v5/matches/${matchId}`, true)
 
+      const champList = matchInfo.info.participants.map(participantInfo => ({
+        name: participantInfo.championName,
+        position: participantInfo.teamPosition,
+        team: participantInfo.teamId
+      }))
+
       // Aggregate Data and get players
-      matchInfo.info.participants.forEach(participantInfo => {
-        const champ = participantInfo.championName
+      matchInfo.info.participants.forEach((participantInfo, index) => {
+        const champName = champList[index].name
+        let champBlock = aggregate[champName] // Block in the Aggregate that contains information about a specific champion
+        const champDidWin = participantInfo.win
         
-        // Aggregate Data
-        if (aggregate[champ]) {
-          aggregate[champ].total++
-          if(participantInfo.win) aggregate[champ].win++
+        // Aggregate General Win/Loss
+        if (champBlock) {
+          // General Win/Loss
+          champBlock.total++
+          if(champDidWin) champBlock.win++
         }
         else {
-          aggregate[champ] = {
+          aggregate[champName] = {
             total: 1,
-            win: participantInfo.win ? 1 : 0
+            win: champDidWin ? 1 : 0,
+            positions: {},
+            matchups: {}
+          }
+          champBlock = aggregate[champName]
+        }
+
+        // Aggregate Position Info
+        const currentPosition = participantInfo.teamPosition // Position that the player played the champion in
+
+        if (!champBlock.positions[currentPosition]) {
+          champBlock.positions[currentPosition] = {
+            total: 0,
+            win: 0,
+            matchups: {}
           }
         }
+
+        const positionBlock = champBlock.positions[currentPosition] // Block in Aggregate that contains information for champion played in a specific position
+        positionBlock.total++
+        if (champDidWin) positionBlock.win++
+
+        // Find opponent player of the same role and add to positional tally
+        for (const champ of champList) {
+          if (champ.position === currentPosition && champ.team !== participantInfo.teamId) {
+            const matchupBlock = positionBlock.matchups[champ.name]
+            if (matchupBlock) {
+              matchupBlock.total++
+              if (champDidWin) matchupBlock.win++
+            }
+            else {
+              positionBlock.matchups[champ.name] = {
+                total: 1,
+                win: champDidWin ? 1 : 0
+              }
+            }
+            break
+          }
+        }
+
+        // Find opponent players in general (Full matchups, ignoring positions played)
+        champList.forEach(champ => {
+          if(champ.team !== participantInfo.teamId) {
+            const matchupBlock = champBlock.matchups[champ.name]
+            if (matchupBlock) {
+              matchupBlock.total++
+              if (champDidWin) matchupBlock.win++
+            }
+            else {
+              champBlock.matchups[champ.name] = {
+                total: 1,
+                win: champDidWin ? 1 : 0
+              }
+            }
+          }
+        })
 
         // Get more player
         const newId = participantInfo.puuid
@@ -95,3 +157,4 @@ spiderSearch(config.initialPlayer, config.maxMatches)
   const str = JSON.stringify(res, null, 2)
   fs.writeFileSync('./output.json', str)
 })
+.catch(err => console.error(err))
